8dc6a8536c37570c8d67035544a575d3d37056c0
MeCuhp <phuc.nm06025@sinhvien.hoasen.edu.vn>
2025-02-28 22:26:44 +0700

    feat: add new components search

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon
+  ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
