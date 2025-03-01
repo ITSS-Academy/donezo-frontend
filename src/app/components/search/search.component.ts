5727ddfcb71dbcb0e9bc567bf1b48d8201da1d9c
MeCuhp <phuc.nm06025@sinhvien.hoasen.edu.vn>
2025-03-01 17:03:03 +0700

    feat: add new search

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import {MatIcon} from '@angular/material/icon';
+import {MatFormField} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {MaterialModule} from '../../shared/modules/material.module';
 
 @Component({
   selector: 'app-search',
   standalone: true,
   imports: [
-    MatIcon
+    MatIcon,
+    MatFormField,
+    MaterialModule
   ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
