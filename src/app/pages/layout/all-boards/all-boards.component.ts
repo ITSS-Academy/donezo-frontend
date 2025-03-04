1465c01df942a63071aaeb0c9facf1ae568ae643
ZenZero <thanh.nt05903@sinhvien.hoasen.edu.vn>
2025-03-04 17:04:46 +0700

    feat: add: create board, all board

--- src/app/pages/layout/all-boards/all-boards.component.ts
+++ src/app/pages/layout/all-boards/all-boards.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
+import {MatMenu, MatMenuItem} from '@angular/material/menu';
+
 
 @Component({
   selector: 'app-all-boards',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton,
+    MatIcon,
+    MatMenu,
+    MatMenuItem
+  ],
   templateUrl: './all-boards.component.html',
   styleUrl: './all-boards.component.scss'
 })
