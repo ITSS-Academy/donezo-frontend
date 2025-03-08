722c98302fbd32e300de091b064d106671f642f1
hhaoz <hao.tt03643@sinhvien.hoasen.edu.vn>
2025-03-08 14:45:18 +0700

    feat: update UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,11 @@ export const routes: Routes = [
   },
   {
     path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
     canActivate: [authGuard],
     loadChildren: () => import('./pages/layout/layout.routes').then(m => m.LayoutRoutes),
   },
