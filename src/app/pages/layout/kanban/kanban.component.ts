7d45a03d58d12a4f7f64e01179a2406272af64fa
MeCuhp <phuc.nm06025@sinhvien.hoasen.edu.vn>
2025-03-08 15:47:10 +0700

    share

--- src/app/pages/layout/kanban/kanban.component.ts
+++ src/app/pages/layout/kanban/kanban.component.ts
@@ -1,15 +1,42 @@
 import {Component} from '@angular/core';
 import {ListTasksComponent} from "./components/list-tasks/list-tasks.component";
 import {MatButton} from '@angular/material/button';
+import {MembersComponent} from './components/add-members/members.component';
+import {MatDialog} from '@angular/material/dialog';
+import {MatIcon} from '@angular/material/icon';
+import {CreateTagsComponent} from './components/create-tags/create-tags.component';
 
 @Component({
   selector: 'app-kanban',
   standalone: true,
-  imports: [ListTasksComponent, MatButton],
+  imports: [ListTasksComponent, MatButton, MatIcon],
   templateUrl: './kanban.component.html',
   styleUrl: './kanban.component.scss'
 })
 export class KanbanComponent {
 
   protected readonly open = open;
+
+  constructor(public dialog: MatDialog) {
+
+  }
+
+  openTagsDialog(): void {
+    const dialogRef = this.dialog.open(CreateTagsComponent, {
+      data: {},
+      width: 'fit-content'
+    });
+  }
+
+  openMembersDialog(): void {
+    const dialogRef = this.dialog.open(MembersComponent, {
+      data: {},
+      width: 'fit-content'
+    });
+  }
+
+
+
+  protected readonly MembersComponent = MembersComponent;
+  protected readonly CreateTagsComponent = CreateTagsComponent;
 }
